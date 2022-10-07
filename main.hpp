085cfa016f17c7b97099b6a1f80d3cad8dd0c5e5
Kyuwoong Lee <kwooler@gmail.com>
2022-10-07 00:52:30 -0700

    Update main.hpp

--- main.hpp
+++ main.hpp
@@ -9,35 +9,12 @@ void bubble(int[], int);
 void printout(int[], int);
 const int SIZE = 100;
 
-// int makearray(int number[])
-// {
-// }
-// void bubble(int number[], int last)
-// {
-// }
-// void printout(int number[], int last)
-// {
-// }
-
 int makearray(int number[])
 {
-	int last;
-	srand(time(NULL));
-	last = rand() % 10 + 10; // from 10 to 19
-
-	for (int i = 0; i < last; i++)
-		number[i] = rand() % 100;
-	return last;
 }
 void bubble(int number[], int last)
 {
-	for (int i = 0; i < last - 1; i++)
-		if (number[i] > number[i + 1])
-			swap(number[i], number[i + 1]);
 }
 void printout(int number[], int last)
 {
-	for (int i = 0; i < last; i++)
-		cout << setw(5) << number[i];
-	cout << endl;
 }
