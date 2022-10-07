8086d052bfa8a427c50b42de2815d626a4dbe6c6
Kyuwoong Lee <kwooler@gmail.com>
2022-10-07 00:30:42 -0700

    Initial commit

---
+++ main.hpp
@@ -0,0 +1,11 @@
+#include <cstdlib>
+#include <ctime>
+#include <iostream>
+using namespace std;
+
+int getRdnum(void);
+int getRdnum(int);
+int getRdnum(int, int);
+
+// Complete overloading functions.
+// Your code here.
