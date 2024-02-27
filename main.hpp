59add33449191b012b5cd16cc68fd7ddd25416be
Ernesto Flores <eflores73@students.avc.edu>
2024-02-27 15:19:10 -0800

    Finished

--- main.hpp
+++ main.hpp
@@ -11,10 +11,28 @@ const int SIZE = 100;
 
 int makearray(int number[])
 {
+    int last;
+    srand(time(0));
+    last = rand() % 11 + 10;
+    for (int i=0; i<last; i++) {
+        number[i] =rand() % 100;
+    }
+    return last;
 }
+
 void bubble(int number[], int last)
 {
+    for(int i=0; i<last-1; i++){
+        if(number[i] > number[i+1]) {
+            swap(number[i], number[i+1]);
+        }
+    }
 }
+
 void printout(int number[], int last)
 {
+    for(int i=0; i<last; i++) {
+        cout << setw(4) << number[i];
+    }    
+    cout << endl;    
 }
